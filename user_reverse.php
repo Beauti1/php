051143e0048de7956d328b6b8ce4ca00359c410d
Beauti1 <91016883+Beauti1@users.noreply.github.com>
2022-06-03 14:06:13 +0530

    user input string in reverse

---
+++ user_reverse.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> User input in reverse </title>
+</head>
+<body>
+	<form name="form" action="user_reverse1.php" method="post">
+		<label>Enter a string</label>
+		<input type="text" name="name"> 
+		<br>
+		<input type="submit">
+	</form>
+</body>
+</html>
